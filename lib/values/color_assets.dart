d9d1be1a9d9236eaaecd962b606d98a6f3836b99
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-07-22 10:04:24 +0700

    Create color_assets file
    Create color_assets file for primary, accent and text color asset

---
+++ lib/values/color_assets.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorAssets {
+  static final Color primarySwatchColor = Color(0xFF827FF0);
+  static final Color accentColor = Color(0xFF5996B4);
+  static final Color primaryTextColor = Colors.grey[900];
+}
